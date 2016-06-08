885c7b04b7892729fa4e1e4ffd1ddedd0466d9fb
Lauren <laurencaverly1@gmail.com>
2016-06-08 21:48:48 +0200

    created table view cells

---
+++ iXplore2/tableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  tableViewCell.swift
+//  iXplore2
+//
+//  Created by Lauren Caverly on 6/8/16.
+//  Copyright © 2016 Lauren Caverly. All rights reserved.
+//
+
+import UIKit
+
+class tableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
